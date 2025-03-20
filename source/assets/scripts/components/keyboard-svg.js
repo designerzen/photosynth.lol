c9059c4ad8eee5d46062e025d84f02c2caea7279
zen <designerzen@gmail.com>
2025-03-20 17:22:13 +0000

    feat: Royal Society assets

---
+++ source/assets/scripts/components/keyboard-svg.js
@@ -0,0 +1,228 @@
+import AbstractInteractive from "./abstract-interactive"
+
+/**
+ * Get the note name (in scientific notation) of the given midi number
+ *
+ * It uses MIDI's [Tuning Standard](https://en.wikipedia.org/wiki/MIDI_Tuning_Standard)
+ * where A4 is 69
+ *
+ * This method doesn't take into account diatonic spelling. Always the same
+ * pitch class is given for the same midi number.
+
+ * @param {Integer} midi - the midi number
+ * @return {String} the pitch
+ */
+export const convertNoteNameToMIDINoteNumber = name => NOTE_NAME_MAP[name]
+
+
+export default class SVGKeyboard extends AbstractInteractive{
+
+	static uniqueID = 0
+
+	isTouching = false
+	keyElements = []
+	htmlElement
+
+	firstNoteNumber = 0
+
+    notes = []
+
+	get svg(){
+		return this.svgString
+	}
+
+	get asElement(){
+		return this.htmlElement
+	}
+
+	constructor( notes, noteOn, noteOff ){
+		super()
+        const unique = `keyboard-${SVGKeyboard.uniqueID++}`
+		this.titleID = `${unique}-title`
+		this.descriptionID = `${unique}-desc`
+		const {svg, whiteKeyElements, blackKeyElements} = this.createKeyboard(notes)
+		this.htmlElement = document.createElement("div")
+		this.htmlElement.className = "piano"
+		this.htmlElement.setAttribute( "data-piano", true )
+		this.htmlElement.innerHTML = svg
+		this.keyElements = Array.from(this.htmlElement.querySelectorAll(".piano-key"))
+		this.firstNoteNumber = notes[0].noteNumber
+		this.svgString = svg
+		this.addInteractivity( this.keyElements, noteOn, noteOff)
+	}
+
+    getNoteFromKey( button){
+        const noteNumber = parseInt( button.getAttribute("data-attribute-number") )
+        const note = this.notes[noteNumber - this.firstNoteNumber]
+        console.info("noteNumber", noteNumber, note, this.notes )
+        return note
+    }
+
+	createBlackKey( key, x, y, r=1.5, width=13, height=80 ){
+		return `<rect 
+					x="${x}" 
+					y="${y}" 
+					rx="${r}" 
+					class="piano-key piano-key-black" 
+					width="${width}" height="${height}" 
+					title="${key.noteName}" 
+                    data-attribute-key="${key.noteKey}" 
+					data-attribute-note="${key.noteName}" 
+					data-attribute-number="${key.noteNumber}" 
+					data-attribute-frequency="${key.frequency}"
+					data-attribute-key="${key.noteKey}"
+					data-attribute-octave="${key.octave}"
+					>
+				</rect>`
+	}
+	
+	createWhiteKey( key, x, y, r=1.5, width=23, height=120 ){
+		return `<rect 
+					x="${x}" 
+					y="${y}" 
+					rx="${r}" 
+					class="piano-key piano-key-white" 
+					width="${width}" height="${height}" 
+					title="${key.noteName}" 
+					data-attribute-key="${key.noteKey}" 
+					data-attribute-note="${key.noteName}" 
+					data-attribute-number="${key.noteNumber}" 
+					data-attribute-frequency="${key.frequency}"
+					data-attribute-octave="${key.octave}"
+					>
+				</rect>`
+	}
+	
+	/**
+	 * 
+	 * @param {Object} key 
+	 * @param {Number} x 
+	 * @param {Number} y 
+	 * @param {Number} r 
+	 * @returns 
+	 */
+	createIndicator( key, x, y, r=5 ){
+		return `<circle 
+					cx="${x}" 
+					cy="${y}" 
+					r="${r}" 
+					class="piano-note-indicator" 
+					data-attribute-note="${key.noteName}" 
+					data-attribute-number="${key.noteNumber}" 
+					data-attribute-frequency="${key.frequency}"
+					data-attribute-octave="${key.octave}"
+					>
+				</circle>`
+	}
+
+	/**
+	 * Create an SVG keyboard
+	 * @param {Array} keys 
+	 * @param {Number} blackKeyWidth 
+	 * @param {Number} whiteKeyWidth 
+	 * @returns {SVG}
+	 */
+	createKeyboard( keys, blackKeyWidth=13, whiteKeyWidth=23, indicatorWidth=8, blackKeyScale=0.5, whiteKeyHeight=140, x=0, y=20 ){
+
+		const curvedRadius = 6
+		const halfBlackKeyWidth = blackKeyWidth / 2
+		const halfIndicatorWidth = indicatorWidth / 2
+		const indicatorRadius = halfIndicatorWidth
+		const spaceBetweenIndicators = whiteKeyWidth - indicatorWidth
+		const blackKeyHeight = whiteKeyHeight * blackKeyScale
+		const totalHeight = whiteKeyHeight
+		
+		// rescaled by SVG - just for proportion
+		let totalWidth = 0 // 1197.8
+		
+		// Keys as strings
+		const whiteKeyElements = []
+		const blackKeyElements = []
+		const keyElements = keys.map( (key,index)=>{
+			
+            // FIXME: 
+			const isBlack = key.accidental ?? false
+			
+			// if the key is a black key, we move back
+			x -= isBlack ? halfBlackKeyWidth : 0
+		
+			const keyElement = isBlack ?
+							this.createBlackKey(key, x, y, curvedRadius, blackKeyWidth, blackKeyHeight) :
+							this.createWhiteKey(key, x, y, curvedRadius, whiteKeyWidth, whiteKeyHeight )
+			
+			const keyQuantity = isBlack ?
+			 				blackKeyElements.push( keyElement ) :
+			 				whiteKeyElements.push( keyElement )
+			
+
+			x += isBlack ? halfBlackKeyWidth : whiteKeyWidth
+			// x += isBlack ? -halfBlackKeyWidth  : whiteKeyWidth
+			
+			// only white keys affect toal width
+			totalWidth += isBlack ? 0 : whiteKeyWidth
+
+            this.notes.push( key )
+
+			return keyElement
+		})
+
+		// Indicators
+		x = 0 // spaceBetweenIndicators / 2
+		y = 10
+
+		const indicatorElements = keys.map( (key,index)=>{
+			const indicator = this.createIndicator(key, x, y, indicatorRadius )
+			x += spaceBetweenIndicators
+			return indicator
+		})
+
+		const keyboard =   `<g class="piano-key-notes piano-keys-white">${whiteKeyElements.join("")}</g>
+							<g class="piano-key-notes piano-keys-black">${blackKeyElements.join("")}</g>`
+
+		const indicators = `<g class="piano-key-indicators">${indicatorElements.join("")}</g>`
+		
+		const svg = `<svg 
+					xmlns="http://www.w3.org/2000/svg" 
+					class="piano-keys" 
+					viewBox="0 0 ${totalWidth} ${totalHeight}" 
+					aria-labelledby="${this.titleID} ${this.descriptionID}"
+					draggable="false">
+					<title id="${this.titleID}">Piano Keyboard with ${keys.length} keys</title>
+					<desc id="${this.descriptionID}">Interactive Piano Keyboard with ${keys.length} keys</desc>
+					${keyboard}
+					${indicators}
+				</svg>`
+
+		return {svg, keyElements, whiteKeyElements, blackKeyElements}
+	}
+
+	/**
+	 * 
+	 * @param {Number} noteNumber 
+	 */
+	setKeyAsActive( noteNumber ){
+		const key = this.htmlElement.querySelector('[data-attribute-number="'+noteNumber+'"]')
+		// const key = this.keyElements[noteNumber - this.firstNoteNumber]
+		if (key)
+		{
+			key.classList.toggle("active", true)
+		}else{
+			console.warn("Key "+noteNumber+" not found")
+		}
+	}
+
+	/**
+	 * 
+	 * @param {Number} noteNumber 
+	 */
+	setKeyAsInactive( noteNumber ){
+		const key = this.htmlElement.querySelector('[data-attribute-number="'+noteNumber+'"]')
+		// const key = this.keyElements[noteNumber - this.firstNoteNumber]
+		if (key)
+		{
+			key.classList.toggle("active", false)
+		}else{
+			console.warn("Key "+noteNumber+" not found")
+		}
+	}
+}
