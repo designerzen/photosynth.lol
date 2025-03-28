4f3b70fbb8331fcb94446ee756810b0585f70fb1
zen <designerzen@gmail.com>
2025-03-28 10:49:44 +0000

    feature: Periodic Wave data for GM instruments

--- source/assets/scripts/instruments/wave-tables.js
+++ source/assets/scripts/instruments/wave-tables.js
@@ -1,223 +1,36 @@
-
-import SAW from "url:/static/wave-tables/01_Saw"
-import TRIANGLE from "url:/static/wave-tables/02_Triangle"
-import SQUARE from "url:/static/wave-tables/03_Square"
-import NOISE from "url:/static/wave-tables/04_Noise"
-import PULSE from "url:/static/wave-tables/05_Pulse"
-import WARM_SAW from "url:/static/wave-tables/06_Warm_Saw"
-import WARM_TRIANGLE from "url:/static/wave-tables/07_Warm_Triangle"
-import WARM_SQUARE from "url:/static/wave-tables/08_Warm_Square"
-import DROPPED_SAW from "url:/static/wave-tables/09_Dropped_Saw"
-import DROPPED_SQUARE from "url:/static/wave-tables/10_Dropped_Square"
-
-import BASS from "url:/static/wave-tables/Bass"
-import BASS_AMP360 from "url:/static/wave-tables/Bass_Amp360"
-import BASS_FUZZ from "url:/static/wave-tables/Bass_Fuzz"
-import BASS_FUZZ_2 from "url:/static/wave-tables/Bass_Fuzz_2"
-import BASS_SUB_DUB from "url:/static/wave-tables/Bass_Sub_Dub"
-import BASS_SUB_DUB_2 from "url:/static/wave-tables/Bass_Sub_Dub_2"
-import BRASS from "url:/static/wave-tables/Brass"
-import BRIT_BLUES from "url:/static/wave-tables/Brit_Blues"
-import BRIT_BLUES_DRIVEN from "url:/static/wave-tables/Brit_Blues_Driven"
-import BUZZY_1 from "url:/static/wave-tables/Buzzy_1"
-import BUZZY_2 from "url:/static/wave-tables/Buzzy_2"
-import CELESTE from "url:/static/wave-tables/Celeste"
-import CHORUS_STRINGS from "url:/static/wave-tables/Chorus_Strings"
-import DISSONANT_PIANO from "url:/static/wave-tables/Dissonant_Piano"
-import DISSONANT_1 from "url:/static/wave-tables/Dissonant_1"
-import DISSONANT_2 from "url:/static/wave-tables/Dissonant_2"
-import DYNA_EP_BRIGHT from "url:/static/wave-tables/Dyna_EP_Bright"
-import DYNA_EP_MED from "url:/static/wave-tables/Dyna_EP_Med"
-import ETHNIC_33 from "url:/static/wave-tables/Ethnic_33"
-import FULL_1 from "url:/static/wave-tables/Full_1"
-import FULL_2 from "url:/static/wave-tables/Full_2"
-import GUITAR_FUZZ from "url:/static/wave-tables/Guitar_Fuzz"
-import HARSH from "url:/static/wave-tables/Harsh"
-import MKL_HARD from "url:/static/wave-tables/Mkl_Hard"
-import ORGAN_2 from "url:/static/wave-tables/Organ_2"
-import ORGAN_3 from "url:/static/wave-tables/Organ_3"
-import PHONEME_AH from "url:/static/wave-tables/Phoneme_ah"
-import PHONEME_BAH from "url:/static/wave-tables/Phoneme_bah"
-import PHONEME_EE from "url:/static/wave-tables/Phoneme_ee"
-import PHONEME_O from "url:/static/wave-tables/Phoneme_o"
-import PHONEME_OOH from "url:/static/wave-tables/Phoneme_ooh"
-import PHONEME_POP_AHHHS from "url:/static/wave-tables/Phoneme_pop_ahhhs"
-import PIANO from "url:/static/wave-tables/Piano"
-import PUTNEY_WAVERING from "url:/static/wave-tables/Putney_Wavering"
-import TB303 from "url:/static/wave-tables/TB303"
-import THROATY from "url:/static/wave-tables/Throaty"
-import TROMBONE from "url:/static/wave-tables/Trombone"
-import TWELVE_STRING_GUITAR_1 from "url:/static/wave-tables/Twelve_String_Guitar"
-import TWELVE_OPTINES from "url:/static/wave-tables/Twelve_OpTines"
-import WURLITZER from "url:/static/wave-tables/Wurlitzer"
-import WURLITZER_2 from "url:/static/wave-tables/Wurlitzer_2"
-
-export const WAVE_TABLE_LOCATIONS = [
-    SAW,
-    TRIANGLE,
-    SQUARE,
-    NOISE,
-    PULSE,
-    WARM_SAW,
-    WARM_TRIANGLE,
-    WARM_SQUARE,
-    DROPPED_SAW,
-    DROPPED_SQUARE,
-    BASS,
-    BASS_AMP360,
-    BASS_FUZZ,
-    BASS_FUZZ_2,
-    BASS_SUB_DUB,
-    BASS_SUB_DUB_2,
-    BRASS,
-    BRIT_BLUES,
-    BRIT_BLUES_DRIVEN,
-    BUZZY_1,
-    BUZZY_2,
-    CELESTE,
-    CHORUS_STRINGS,
-    DISSONANT_PIANO,
-    DISSONANT_1,
-    DISSONANT_2,
-    DYNA_EP_BRIGHT,
-    DYNA_EP_MED,
-    ETHNIC_33,
-    FULL_1,
-    FULL_2,
-    GUITAR_FUZZ,
-    HARSH,
-    MKL_HARD,
-    ORGAN_2,
-    ORGAN_3,
-    PHONEME_AH,
-    PHONEME_BAH,
-    PHONEME_EE,
-    PHONEME_O,
-    PHONEME_OOH,
-    PHONEME_POP_AHHHS,
-    PIANO,
-    PUTNEY_WAVERING,
-    TB303,
-    THROATY,
-    TROMBONE,
-    TWELVE_STRING_GUITAR_1,
-    TWELVE_OPTINES,
-    WURLITZER,
-    WURLITZER_2
-]
-export const WAVE_FORM_NAMES = [
-    "Saw",
-    "Triangle",
-    "Square",
-    "Noise",
-    "Pulse",
-    "Warm_Saw",
-    "Warm_Triangle",
-    "Warm_Square",
-    "Dropped_Saw",
-    "Dropped_Square",
-    "Bass",
-    "Bass_Amp360",
-    "Bass_Fuzz",
-    "Bass_Fuzz_ 2",
-    "Bass_Sub_Dub",
-    "Bass_Sub_Dub_2",
-    "Brass",
-    "Brit_Blues",
-    "Brit_Blues_Driven",
-    "Buzzy_1",
-    "Buzzy_2",
-    "Celeste",
-    "Chorus_Strings",
-    "Dissonant Piano",
-    "Dissonant_1",
-    "Dissonant_2",
-    "Dyna_EP_Bright",
-    "Dyna_EP_Med",
-    "Ethnic_33",
-    "Full_1",
-    "Full_2",
-    "Guitar_Fuzz",
-    "Harsh",
-    "Mkl_Hard",
-    "Organ_2",
-    "Organ_3",
-    "Phoneme_ah",
-    "Phoneme_bah",
-    "Phoneme_ee",
-    "Phoneme_o",
-    "Phoneme_ooh",
-    "Phoneme_pop_ahhhs",
-    "Piano",
-    "Putney_Wavering",
-    "TB303",
-    "Throaty",
-    "Trombone",
-    "Twelve String Guitar 1",
-    "Twelve_OpTines",
-    "Wurlitzer",
-    "Wurlitzer_2",
-]
-
-
-import BANJO from "url:/static/wave-tables/general-midi/banjo"
-import DULCIMER from "url:/static/wave-tables/general-midi/dulcimer"
-import GUITAR from "url:/static/wave-tables/general-midi/guitar"
-import HARP from "url:/static/wave-tables/general-midi/harp"
-import KOTO from "url:/static/wave-tables/general-midi/koto"
-import SHAMISEN from "url:/static/wave-tables/general-midi/shamisen"
-
-export const WAVE_TABLE_LOCATIONS_B = [
-    BANJO,
-    DULCIMER,
-    GUITAR,
-    HARP,
-    KOTO,
-    SHAMISEN
-]
-
-export const WAVE_FORM_NAMES_B = [
-    "Banjo",
-    "Dulcimer",
-    "Guitar",
-    "Harp",
-    "Koto",
-    "Shamisen"
-]
-
-const ALL_WAVE_FORM_NAMES = [
-    ...WAVE_FORM_NAMES,
-    ...WAVE_FORM_NAMES_B
-]
-
+import { WAVE_FORM_NAMES_GENERAL_MIDI, WAVE_TABLE_LOCATIONS_GENERAL_MIDI } from "./tables/wave-table-general-midi"
+import { WAVE_FORM_NAMES_GOOGLE, WAVE_TABLE_LOCATIONS_GOOGLE } from "./tables/wave-table-google"
 
 const waveTables = new Map()
-
 const waveTableNames = new Map()
+const ALL_WAVE_FORM_NAMES = []
 
 const assignWaveTables = ( locations, waves, waveMap ) => {
     // now try and create a relationship database...
     locations.forEach( (waveTableName, index) => {
         // waveTables.set(waveTableName, WAVE_FORM_NAMES[index])
-        const nameExpanded = waves[index].replaceAll("_"," ")
-        waveMap.set(waves[index], waveTableName)
-        waveMap.set(waves[index].toLowerCase(), waveTableName)
+        const name = waves[index]
+        const nameExpanded = name.replaceAll("_"," ")
+        waveMap.set(name, waveTableName)
+        waveMap.set(name.toLowerCase(), waveTableName)
         waveMap.set(nameExpanded, waveTableName)
         waveMap.set(nameExpanded.toLowerCase(), waveTableName)
+        ALL_WAVE_FORM_NAMES.push(name)
         // console.info("waveTableName", waveTableName, WAVE_FORM_NAMES[index] ) 
     }) 
 }
 
 // Google Wave tables from :
 // https://github.com/GoogleChromeLabs/web-audio-samples/blob/main/src/demos/wavetable-synth/index.html
-assignWaveTables(WAVE_TABLE_LOCATIONS, WAVE_FORM_NAMES, waveTableNames)
+assignWaveTables(WAVE_TABLE_LOCATIONS_GOOGLE, WAVE_FORM_NAMES_GOOGLE, waveTableNames)
+assignWaveTables(WAVE_TABLE_LOCATIONS_GENERAL_MIDI, WAVE_FORM_NAMES_GENERAL_MIDI, waveTableNames)
+console.info("waveTableNames", {waveTableNames, ALL_WAVE_FORM_NAMES} ) 
 
-// 
-assignWaveTables(WAVE_TABLE_LOCATIONS_B, WAVE_FORM_NAMES_B, waveTableNames)
-
-console.info("waveTableNames", waveTableNames ) 
-
-// Pick from the options above
+/**
+ * Pick from the options above
+ * @param {String} waveTableName 
+ * @returns 
+ */
 export const loadWaveTableFromFile = async (waveTableName=TB303) => { 
     const url = waveTableNames.get(waveTableName)
     // we use the names from above...
@@ -251,7 +64,11 @@ export const loadWaveTableFromFile = async (waveTableName=TB303) => {
     return waves
 }
 
-
+/**
+ * Load a wave table from the file system and cache
+ * @param {String} waveTableName 
+ * @returns 
+ */
 export const loadWaveTable = async (waveTableName=TB303) => { 
     // Cache in static Map and load if registered
     if (waveTables.has(waveTableName) ){
@@ -261,9 +78,18 @@ export const loadWaveTable = async (waveTableName=TB303) => {
     }
 }
 
+/**
+ * 
+ * @returns 
+ */
 export const getRandomWaveTableName = () => { 
     return ALL_WAVE_FORM_NAMES[Math.floor(Math.random() * ALL_WAVE_FORM_NAMES.length)]
 }
+
+/**
+ * 
+ * @returns 
+ */
 export const loadRandomWaveTable = async () => { 
     return loadWaveTable(getRandomWaveTableName())
 }
