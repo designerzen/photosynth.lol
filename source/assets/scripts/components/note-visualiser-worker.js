661b3e47ca3fb4d4ecc83f60273103760c33fe3b
zen <designerzen@gmail.com>
2025-04-11 20:12:16 +0100

    feat: note visualiser

--- source/assets/scripts/components/note-visualiser-worker.js
+++ source/assets/scripts/components/note-visualiser-worker.js
@@ -5,7 +5,10 @@ let firstNoteNumber
 
 let loop = false
 let vertical = false
+let noteOnCount = 0
 let wave = 0
+
+let lastNoteColour
 
 // Canvases
 let canvas
@@ -63,6 +66,7 @@ function renderHorizontal(time) {
     // console.info("renderHorizontal", mirror.width, mirror.height, canvas.width, canvas.height )
 }
 
+
 function render() {
     vertical ? 
         renderVertical(counter) : 
@@ -76,8 +80,7 @@ function render() {
     //     Math.random() * 400, 
     //     Math.random() * 400, 
     //     Math.random() * 400
-    // )
-
+    
     if (loop) {
         requestAnimationFrame(render)
     }
@@ -103,7 +106,6 @@ onmessage = (evt) => {
     {
         canvas = evt.data.canvas
         context = canvas.getContext('2d')
-
         notes = evt.data?.notes ?? []
         firstNoteNumber = notes[0].number
         // console.info("firstNoteNumber", firstNoteNumber, notes[0], {notes})
@@ -120,8 +122,8 @@ onmessage = (evt) => {
         loop = true
         render()
         
-        // console.info("mirror created", canvas.width, canvas.height , notes.length, {firstNoteNumber, mirror, canvas, context, mirrorContext, noteSize, notes })
-
+        console.info("mirror created", canvas.width, canvas.height , notes.length, {firstNoteNumber, mirror, canvas, context, mirrorContext, noteSize, notes })
+        
         return
     }
      
@@ -133,17 +135,20 @@ onmessage = (evt) => {
     {
         case "noteOn":
             context.fillStyle = evt.data.colour
+            lastNoteColour = evt.data.colour
             if (vertical)
             {
                 context.fillRect( transposedNoteNumber * noteSize, canvas.height - noteDepth, noteSize, noteDepth )
             }else{
                 context.fillRect( 0, transposedNoteNumber * noteSize, noteDepth, noteSize ) 
             }
+            noteOnCount++
             // console.info("VIZ:noteOn", evt.data.note, {firstNoteNumber, transposedNoteNumber}, notes[0], {notes} ) 
             break
 
         case "noteOff":
             context.fillStyle = VISUALISER_OPTIONS.backgroundColour
+            lastNoteColour = VISUALISER_OPTIONS.backgroundColour
             if (vertical)
             {
                 // context.clearRect( transposedNoteNumber * noteSize, canvas.height - gap, noteSize, gap )
@@ -152,7 +157,14 @@ onmessage = (evt) => {
                 // context.clearRect( 0, transposedNoteNumber * noteSize, gap, noteSize ) 
                 context.fillRect( 0, transposedNoteNumber * noteSize, gap, noteSize ) 
              }
+             noteOnCount--
             //console.info("VIZ:noteOff", evt.data, transposedNoteNumber )
+            break
+
+        case "coord":
+            mouseX = evt.data.x
+            mouseY = evt.data.y
+            mouseDown = evt.data.pressed
             break
 
         case "resize":
--- source/assets/scripts/components/note-visualiser.js
+++ source/assets/scripts/components/note-visualiser.js
@@ -2,19 +2,29 @@
  * Scrolling note on / off visualisation
  */
 
-import HTML_CANVAS_WORKER from "url:./offscreen-canvas.js"
+import NOTE_VISUALISER_CANVAS_WORKER from "url:./note-visualiser-worker.js"
+import { AbstractResizeable } from "./abstract-resizeable-canvas.js"
 
-export default class NoteVisualiser{
+export default class NoteVisualiser extends AbstractResizeable{
 
     notes
     canvas
     context
-    width
+
     height
     counter = 0
+    notesOn = 0
+
     started = false
+    mouseDown = false
+
+    mouseX = 0
+    mouseY = 0
 
     constructor( notes, canvas, vertical=false, wave=0 ){
+        
+        super(canvas, NOTE_VISUALISER_CANVAS_WORKER, {vertical, notes})
+
         this.notes = notes
         this.canvas = canvas
 
@@ -24,142 +34,29 @@ export default class NoteVisualiser{
         this.wave = wave
         this.vertical = vertical
 
-        this.worker = new Worker(HTML_CANVAS_WORKER)
-        const canvasWorker = this.canvas.transferControlToOffscreen()
-        this.worker.postMessage({ canvas: canvasWorker, notes }, [canvasWorker])
-        
-        this.onResize = this.onResize.bind(this) 
-        const resizeObserver = new ResizeObserver(this.onResize)
-        resizeObserver.observe(canvas, {box: 'content-box'})
-
-        // this.advance = this.advance.bind(this) 
         // start!
         // this.advance()
     }
 
     /**
-     * 
+     * Note On
      * @param {Note} note 
      * @param {number} velocity 
      */
     noteOn( note, velocity=1 ){
-        const truncatedNoteNumber = note.noteNumber //- this.notes[0].noteNumber
-        const payload = { type:"noteOn", note:truncatedNoteNumber, colour:note.colour, velocity, vertical:this.vertical }
-        console.info("NOTEVIZ noteOn", {note, velocity, payload} )
-        this.worker.postMessage(payload)  
+        const payload = { type:"noteOn", note, velocity }
+        // console.info("NOTEVIZ noteOn", {note, velocity, payload} )
+        this.notesOn++
+        this.worker.postMessage(payload)
     }
 
     /**
-     * 
+     * Note Off
      * @param {Note} note 
      * @param {Number} velocity 
      */
     noteOff( note, velocity=1 ){
-        const truncatedNoteNumber = note.noteNumber //- this.notes[0].noteNumber
-        this.worker.postMessage({ type:"noteOff", note:truncatedNoteNumber, velocity, vertical:this.vertical })
-    }
-
-    /**
-     * PUBLIC
-     */
-    advance(){
-
-        const gap = 3
-        const zone = 1
-
-        const lurch = this.wave === 0 ? 
-            0 : 
-            this.wave * (Math.sin(this.counter++ * 0.05))
-
-        console.info(this.wave, "advance", {canvas:this.canvas}, 0, 0, this.width-gap, this.height, gap,lurch, this.width-gap, this.height ) 
-
-        if (this.vertical)
-        {
-            // (image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-            this.context.drawImage( offscreen, 0, gap, this.width, this.height-gap, lurch, 0, this.width, this.height-gap )
-            //this.context.clearRect( 0, this.height-zone, this.width, zone )
-        }else{
-            // (image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-            this.context.clearRect( gap, 0, this.width, this.height )
-
-            this.context.drawImage( offscreen, 0, 0, this.width-gap, this.height, gap, lurch, this.width-gap, this.height  )
-            //this.context.clearRect( 0, 0, zone, this.height )
-        }
-
-        // loop
-        requestAnimationFrame( this.advance )
-    }
-    
-    resizeCanvasToDisplaySize(displayWidth, displayHeight ){
-
-        // console.error(this, "mirror",{ displayWidth, displayHeight })
-        // Get the size the browser is displaying the canvas in device pixels.
-        // Check if the canvas is not the same size.
-        const needResize = this.canvas.width !== displayWidth || this.canvas.height !== displayHeight
-
-        if (needResize) 
-        {
-            this.started = true
-
-         
-            console.error("mirror",{ needResize, displayWidth, displayHeight })
-
-            this.width = displayWidth
-            this.height = displayHeight
-
-            // NB. Make the canvas the same size via OffscreenCanvas
-            //      this.canvas.width  = displayWidth
-            //      this.canvas.height = displayHeight
-            this.worker.postMessage({ type:"resize", displayWidth, displayHeight, vertical:this.vertical })
-        }
-
-        return needResize
-    }
-    
-    onResize(entries) {
-       
-        for (const entry of entries) 
-        {
-            let width
-            let height
-            let dpr = window.devicePixelRatio
-            let dprSupport = false
-
-            if (entry.devicePixelContentBoxSize) {
-                // NOTE: Only this path gives the correct answer
-                // The other paths are an imperfect fallback
-                // for browsers that don't provide anyway to do this
-                width = entry.devicePixelContentBoxSize[0].inlineSize
-                height = entry.devicePixelContentBoxSize[0].blockSize
-                dpr = 1 // it's already in width and height
-                dprSupport = true
-
-            } else if (entry.contentBoxSize) {
-
-                if (entry.contentBoxSize[0]) {
-
-                    width = entry.contentBoxSize[0].inlineSize
-                    height = entry.contentBoxSize[0].blockSize
-
-                } else {
-
-                    // legacy
-                    width = entry.contentBoxSize.inlineSize
-                    height = entry.contentBoxSize.blockSize
-                }
-
-            } else {
-                // legacy
-                width = entry.contentRect.width
-                height = entry.contentRect.height
-            }
-
-            const displayWidth = Math.round(width * dpr)
-            const displayHeight = Math.round(height * dpr)
-            
-            this.resizeCanvasToDisplaySize(displayWidth, displayHeight)
-           
-            console.info(this.wave, "advance", {canvas:this.canvas}, 0, 0, this.width, this.height ) 
-        }
+        this.notesOn--
+        this.worker.postMessage({ type:"noteOff", note, velocity })
     }
 }
