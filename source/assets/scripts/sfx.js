b537ec092af2f90504e33afcf89a76d45a7c84b0
zen <designerzen@gmail.com>
2025-04-23 19:48:26 +0100

    feat: magic spells

--- source/assets/scripts/sfx.js
+++ source/assets/scripts/sfx.js
@@ -1,8 +1,11 @@
 
-import SPELL_ from "url:/source/assets/audio/fx/spells/magic-mallet-6262.mp3"
+import SPELL_MAGIC_MALLET from "url:/source/assets/audio/fx/spells/magic-mallet-6262.mp3"
+import SPELL_MAGIC_CAST from "url:/source/assets/audio/fx/spells/magic-spell-cast-sound-effect-224173.mp3"
+
 
 const SPELLS = [
-    SPELL_
+    SPELL_MAGIC_MALLET,
+    SPELL_MAGIC_CAST
 ]
 
 export const getRandomSpell = () => {
